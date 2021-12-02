94017da93ccec6876d03045bb2afeb5e509ee217
SpaceWolfy <s_scozzari@libero.it>
2021-12-02 15:29:44 +0100

    Set-up of the reasoning in a comment

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* 
+1) Necessito di richiedere all'utente il proprio nome, seguito dal cognome e successivamente 
+dal suo colore preferito.
+
+- Partirò dunque dalla costruzione di tre variabili: 
+le prime due saranno const in quanto racchiuderanno dati personali, 
+la terza sarà una variabile let, ipotizzando il fatto che una preferenza possa variare 
+anche in brevi intervalli di tempo.
+
+- Poiché sto effettuando una richiesta di informazioni, alle variabili sopraccitate assocerò dei prompt()
+
+2) Una volta recuperate le informazioni, dovrò stampare a schermo una password 
+che comprenda i dati richiesti secondo l'ordinamento "nome-cognome-colorepreferito", seguiti dal numero 21;
+
+esempio: nomecognomecolorepreferito21
+
+- Creerò dunque una variabile nominata htmlElement, che conterrà l'oggetto HTML con id: #password
+in cui stamperò le informazioni recuperate.
+*/
